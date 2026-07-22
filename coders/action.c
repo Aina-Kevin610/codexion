59bd06672848a903907bbfdfc1dfca8c66191d63
Aina-Kevin610 <randrianambinintsoaainakevin@gmail.com>
2026-07-22 19:12:59 +0300

    feat: monitor.simualation end detecter

--- coders/action.c
+++ coders/action.c
@@ -3,22 +3,15 @@
 void  compile(t_coder *coder)
 {
   printf("Compiling...\n");
-  sleep(1);
-  printf("%d", coder->dongle_hold);
-  printf("done\n");
 }
 
 void  debug(t_coder *coder)
 {
   printf("Debuging...\n");
-  usleep(2+coder->dongle_hold);
-  printf("done!\n");
 }
 
 void  refactor(t_coder *coder)
 {
   printf("Refactoring...\n");
-  usleep(2+coder->dongle_hold);
-  printf("done!\n");
 }
 
