540ef620381c30642c562079cdf20f6022f944d3
Aina-Kevin610 <randrianambinintsoaainakevin@gmail.com>
2026-09-08 10:11:05 +0300

    debug: remove forgotten code (request part)

--- coders/request.c
+++ coders/request.c
@@ -11,37 +11,3 @@
 /* ************************************************************************** */
 
 #include "codexion.h"
-
-t_request	*create_request(t_coder *coder)
-{
-	t_request	*request;
-
-	request = malloc(sizeof(t_request));
-	if (!request)
-		return (NULL);
-	request->id_coder = coder->id;
-	request->deadline = 0;
-	request->request_time = get_actual_time();
-	request->parent = NULL;
-	request->child = NULL;
-	return (request);
-}
-
-int	request(t_coder *coder)
-{
-	t_request	*req;
-
-	pthread_mutex_lock(&coder->all->lock);
-	req = create_request(coder);
-	if (!req)
-	{
-		pthread_mutex_unlock(&coder->all->lock);
-		return (0);
-	}
-	req->deadline = (int)(coder->last_compile_start + coder->argument->burnout);
-	heap_push(coder->dongle, req, &coder->argument->scheduler);
-	heap_push(coder->prev->dongle, req, &coder->argument->scheduler);
-	coder->request = req;
-	pthread_mutex_unlock(&coder->all->lock);
-	return (1);
-}
